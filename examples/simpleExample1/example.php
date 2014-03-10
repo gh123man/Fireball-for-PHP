c147740d7cdd4b64dfebf89085e352b9b2a644c9
Brian Floersch <gh123man@gmail.com>
2014-03-10 16:08:13 -0400

    added example.php

---
+++ examples/simpleExample1/example.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once 'Person.php';
+include_once '../../src/Fireball.php';
+
+Fireball\ORM::connect('fbSimpleExample', 'localhost', 'DB_USER', 'DB_PASSWORD');
+
+$person = Person::newPerson('John', 'Doe');
+
+echo $person->data->ID(); //print id
+echo "\n";
+
+echo $person->data->fname(); //print first name
+echo "\n";
+
+echo $person->data->fname('Bob'); //set first name to bob
+echo "\n";
+
+echo $person->data->fname(); //print first name
+echo "\n";
+
+echo $person->data->lname(); //print last name
+echo "\n";
+
+echo $person->data->time(); //print timestamp
+echo "\n";
+
+
+
+
+
+
+?>
